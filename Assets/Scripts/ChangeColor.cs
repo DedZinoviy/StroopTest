1eaef00e0fb9dd0bc2ab3096f17d44cf5ea96999
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-28 11:54:06 +0300

    Created script for changing colors in color window.

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    private enum Colors
+    {
+        RED, // Обозначает красный цвет.
+        YELLOW, // Обозначает синий цвет.
+        GREEN, // Обозначает зелёный цвет.
+        BLUE // Обозначает голубой цвет.
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
