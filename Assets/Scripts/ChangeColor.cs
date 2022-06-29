a48e347b8b85fbc7ea6f05eb3b096b15be487f99
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-06-29 10:30:08 +0300

    Added Change Color script for text in window.

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeColor : MonoBehaviour
 {
+    private TMP_Text _text;
     private enum Colors
     {
         RED, // Обозначает красный цвет.
@@ -15,12 +18,20 @@ public class ChangeColor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _text = GetComponent<TMP_Text>();
+        int random = Random.Range(0, 3); // Сгенерировать случайный цвет.
+        Color color = new Color();
+        if (random == 0) color = Color.red;
+        else if (random == 1) { color = Color.yellow; }
+        else if (random == 2) color = Color.green;
+        else if (random == 3) color = Color.blue;
+        //extMeshPro text = new TextMeshPro();
+        _text.color = color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
