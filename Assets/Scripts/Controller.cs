e6bd5624f1ac51f7ae813f0785535738d240f3b9
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-07-05 09:47:52 +0300

    Added commentaries for Contoller script in stroop test.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -12,6 +12,7 @@ public class Controller : MonoBehaviour
     [SerializeField] private TMP_Text TimeEdit;
     [SerializeField] private Bar TimeBar;
     [SerializeField] private SceneChanger Change;
+    [SerializeField] private int QuestionCount;
     private PlayerPrefs save = new PlayerPrefs();
     private double Seconds;
     private double OriginalSec;
@@ -28,46 +29,55 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Seconds > 0)
+        if (Seconds > 0) // Если время не вышло...
         {
             double prevSec = Seconds;
-            Seconds -= Time.deltaTime;
-            TimeEdit.text = "Время: " + Math.Round(Seconds, 1);
+            Seconds -= Time.deltaTime; // Уменьшить оставшееся время.
+            TimeEdit.text = "Время: " + Math.Round(Seconds, 1); // Вывести оставшееся время на экран.
+            
+            // Изменить длину и цвет временной шкалы, согласно времени.
             double percent = Seconds / prevSec;
             double colorPerc = Seconds / OriginalSec;
             TimeBar.ChangeBarSize((float)percent);
             TimeBar.ChangeBarColor((float)colorPerc);
         }
-        else
+        else // Иначе...
         {
-            if (this.Score > save.LoadScore())
+            if (this.Score > save.LoadScore()) // Сохранить результат, если он является новым рекордом.
             {
                 save.HighestScore = this.Score;
                 save.SaveScore();
             }
-            Change.ToMenu();
+            Change.ToMenu(); // Выйти в меню.
         }
 
     }
 
+    /// <summary>
+    /// Method for checking answers correctness.
+    /// </summary>
+    /// <param name="button">Clicked button.</param>
     public void isCorrectAnswer(Btn button)
     {
-        if (EtalonColor.currentColor == button.Color)
+        if (EtalonColor.currentColor == button.Color) // Если цвет кнопки совпадает с цветом на дисплее...
         {
-            Score++;
-            Win();
+            Score++; // Увеличить счёт.
+            Win(); // Проеврить выигрышную ситуацию.
         }
-        else
+        else // Иначе...
         {
-            Seconds -= 0.5;
+            Seconds -= 0.5; // Вычесть время в качестве штрафа.
         }
-        ScoreEdit.text = "Счёт: " + Score;
-        text.SetText();
+        ScoreEdit.text = "Счёт: " + Score; // Отобразить счёт на экране.
+        text.SetText(); // Перемешать кнопки.
     }
 
+    /// <summary>
+    /// Method for checking winning situation.
+    /// </summary>
     private void Win()
     {
-        if (this.Score >= 2)
+        if (this.Score >= QuestionCount) // Перейти к другой головоломке, если счёт совпадает с требуемым.
         {
             Change.NextScene();
         }
