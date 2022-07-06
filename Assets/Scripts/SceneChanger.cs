4945e916c0d95b75acc54e2d861e1e2304ff405d
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-07-06 11:49:11 +0300

    Added comments and descriptions into the scene changer class.

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -5,13 +5,25 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Class for changing scenes in application.
+/// </summary>
 public class SceneChanger : MonoBehaviour
 {
+    /// <summary>
+    /// Inscance of saving module.
+    /// </summary>
     [SerializeField]
     private PlayerPrefs playerPrefs;
 
+    /// <summary>
+    /// List of scenes' names.
+    /// </summary>
     private List<string> scenesSequence;
 
+    /// <summary>
+    /// Instance of randomizer.
+    /// </summary>
     private System.Random random;
 
     // Start is called before the first frame update
@@ -27,21 +39,27 @@ public class SceneChanger : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Loads next random scene.
+    /// </summary>
     public void NextScene()
     {
-        if (scenesSequence.Count <= 0)
+        if (scenesSequence.Count <= 0) // Сгенерировать массив сцен, если он пустой.
             GenerateScenesSequence();
 
-        string nextScene = scenesSequence[0];
-        scenesSequence.RemoveAt(0);
-        playerPrefs.SetScenes(scenesSequence);
-        ChangeScene(nextScene);
+        string nextScene = scenesSequence[0]; // Получить название следующей сцены.
+        scenesSequence.RemoveAt(0); // Считать выбранную сцену использованной.
+        playerPrefs.SetScenes(scenesSequence); // Сохранить изменённую последовательность в логе.
+        ChangeScene(nextScene); // Сменить сцену.
     }
 
+    /// <summary>
+    /// Loads main menu scene.
+    /// </summary>
     public void ToMenu()
     {
-        playerPrefs.ClearScenes();
-        ChangeScene("MainMenu");
+        playerPrefs.ClearScenes(); // Очистить список сцен.
+        ChangeScene("MainMenu"); // Открыть сцену меню.
     }
 
     /// <summary>
@@ -61,21 +79,24 @@ public class SceneChanger : MonoBehaviour
         Application.Quit();
     }
 
+    // OnApplicationQuit is called when the application quits.
     private void OnApplicationQuit()
     {
         playerPrefs.ClearScenes();
     }
 
+    /// <summary>
+    /// Generates random scenes sequence.
+    /// </summary>
     private void GenerateScenesSequence()
     {
-        scenesSequence = new List<string> { "FallingFruits", "Labyrinth", "StroopTest" };
+        scenesSequence = new List<string> { "FallingFruits", "Labyrinth", "StroopTest" };  // Перечислиить все возможные сцены.
 
-        for (int i = scenesSequence.Count - 1; i >= 1; i--)
+        // Создать случайную последовательность.
+        for (int i = scenesSequence.Count - 1; i >= 1; i--) // Для каждой сцены.
         {
-            int newIndex = random.Next(0, i + 1);
-            (scenesSequence[i], scenesSequence[newIndex]) = (scenesSequence[newIndex], scenesSequence[i]);
+            int newIndex = random.Next(0, i + 1); // Выбрать случайное положение сцены.
+            (scenesSequence[i], scenesSequence[newIndex]) = (scenesSequence[newIndex], scenesSequence[i]); // Вставить сцену.
         }
-
-        scenesSequence.ForEach(x => Debug.Log(x));
     }
 }
