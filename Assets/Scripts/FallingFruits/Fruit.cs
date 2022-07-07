eb5e0b2c560015081040c8754bf61fb18715829b
DmitryDemenkov <demon.krut2002@mail.ru>
2022-07-07 14:56:02 +0300

    Add falling fruits in falling fruits

--- Assets/Scripts/FallingFruits/Fruit.cs
+++ Assets/Scripts/FallingFruits/Fruit.cs
@@ -9,7 +9,17 @@ public class Fruit : MonoBehaviour, IPointerDownHandler
     public GameController controller { get; set; }
 
     public Color color;
-    
+
+    [SerializeField]
+    private List<GameObject> sprites;
+
+    private void Start()
+    {
+        System.Random random = new System.Random();
+        GameObject sprite = sprites[random.Next(0, sprites.Count)];
+        sprite.SetActive(true);
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         controller.CatchFruit(color);
